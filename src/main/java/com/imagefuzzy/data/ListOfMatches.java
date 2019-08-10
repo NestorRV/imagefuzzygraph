adaeeedced557061b67af341f51d11229c8fa2fb
NestorRV <nestorrodriguezvico@gmail.com>
2019-08-10 19:00:53 +0200

    Add ListOfMatches class

---
+++ src/main/java/com/imagefuzzy/data/ListOfMatches.java
@@ -0,0 +1,11 @@
+package com.imagefuzzy.data;
+
+import java.util.ArrayList;
+
+/**
+ * Class representing a list of matches.
+ *
+ * @author @author Néstor Rodríguez Vico (nrv23@correo.ugr.es).
+ */
+public class ListOfMatches extends ArrayList<Tuple<String, String>> {
+}
